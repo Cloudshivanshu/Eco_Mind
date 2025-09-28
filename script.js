6b620b51c12d61655eb65ab0ea65cbaf175f78ec
Ujjawal Gupta <ujjawalg74751@gmail.com>
2025-09-28 16:29:46 +0530

    Nav bar and bg done

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const cardsWrapper = document.getElementById('cards');
 
-    function scrollCards(direction) {
-      const cardWidth = cardsWrapper.querySelector('.card').offsetWidth + 20; // card + margin
-      cardsWrapper.scrollBy({
+function scrollCards(direction) {
+    const cardWidth = cardsWrapper.querySelector('.card').offsetWidth + 20; // card + margin
+    cardsWrapper.scrollBy({
         left: direction * cardWidth,
         behavior: 'smooth'
-      });
-    }
-    document.addEventListener("DOMContentLoaded", function () {
-        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-        [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el));
-      });
+    });
+}
+document.addEventListener("DOMContentLoaded", function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el));
+});
