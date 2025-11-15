d9f3c726e20f09b138c6b21777172787699fdbc1
Heavail <satyamkr209@gmail.com>
2025-11-15 21:17:45 +0530

    added main_menu and replaced the difficulty menu with it

--- Main/Main_App/urls.py
+++ Main/Main_App/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from Main_App import views
 urlpatterns = [
     path('',views.index,name = 'Main_App'),
-    path('start/',views.start,name = 'start'),
+    # path('start/',views.start,name = 'start'),
     path('beginner/',views.beginner,name = 'beginner'),
-    path('game/',views.game,name = 'game')
+    path('start/',views.game,name = 'game')
 ]
